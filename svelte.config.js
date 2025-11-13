e6f37d2146ebae2293edcae2bb732b5e7e09774f
MuradAles <muradaleskerov99@gmail.com>
2025-11-13 02:14:29 -0500

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			base: process.env.PUBLIC_BASE_PATH || ''
+		},
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
